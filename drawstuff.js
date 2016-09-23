fd92f13f79e053d6663414c0d6bc82f87df57b11
rgajare <rgajare@ncsu.edu>
2016-09-23 09:03:18 -0400

    triangles updates

--- drawstuff.js
+++ drawstuff.js
@@ -1,6 +1,7 @@
 /* classes */
 // Using mathjs library for doing triangle rendering. Since it has lot of complex maths and will be skipped.
 // Color constructor
+
 class Color {
   constructor(r,g,b,a) {
     try {
@@ -43,10 +44,12 @@ class Color {
 }// end color class
 
 class ViewPane {
-  constructor(width, height, eye, look_up, look_at, d, c) {
+  constructor(width, height, eye, look_up, look_at, d, c, asp_x, asp_y) {
     this.w = width;
     this.h = height;
     this.eye = eye;
+    this.asp_x = asp_x;
+    this.asp_y = asp_y;
     this.look_up = look_up;
     this.look_at = look_at;
     this.d = d;
@@ -55,16 +58,23 @@ class ViewPane {
   }
   // get the real coord of the pixels on view pane
   getRealCoord(coord) {
-    return [ this.ul[0] + (coord[0]/this.w), this.ul[1] - (coord[1]/this.h), this.ul[2]]
+    var n = Maths.unitvector(this.look_at, this.eye);
+    var u = Maths.normalise(math.cross(n, this.look_up));
+    var v = math.cross(u,n);
+    this.c = Maths.minus(this.eye, Maths.mul(this.d,n)); //c=eye-d*n
+    this.ul = Maths.add(Maths.add(this.c, Maths.mul(this.asp_x/2, u)), Maths.mul(this.asp_y/2, v)); // l=c-w/2u-h/2v
+    // var real_coord =
+    return  Maths.minus(Maths.minus(this.ul,Maths.mul(coord[0]*this.asp_x/this.w, u)), Maths.mul(coord[1]*this.asp_y/this.h, v));
+    //return [ this.ul[0] + (coord[0]/this.w), this.ul[1] - (coord[1]/this.h), this.ul[2]];
   }
-
 }
 
 var vp;
 var pixels = [];
-
+var external_lighting;
+var render_triangles;
+var blinn_phong;
 /* utility functions */
-
 // draw a pixel at x,y using color
 function drawPixel(imagedata,x,y,color) {
   try {
@@ -86,14 +96,9 @@ function drawPixel(imagedata,x,y,color) {
   }
 } // end drawPixel
 
-// get the input spheres from the standard class URL
-function getInputSpheres() {
-  const INPUT_SPHERES_URL =
-        "https://ncsucgclass.github.io/prog1/spheres.json";
-
-  // load the spheres file
+function connect(URL) {
   var httpReq = new XMLHttpRequest(); // a new http request
-  httpReq.open("GET",INPUT_SPHERES_URL,false); // init the request
+  httpReq.open("GET",URL,false); // init the request
   httpReq.send(null); // send the request
   var startTime = Date.now();
   while ((httpReq.status !== 200) && (httpReq.readyState !== XMLHttpRequest.DONE)) {
@@ -105,109 +110,33 @@ function getInputSpheres() {
     return String.null;
   } else
     return JSON.parse(httpReq.response);
+}
+
+// get the input spheres from the standard class URL
+function getInputSpheres() {
+  const INPUT_SPHERES_URL =
+        "https://ncsucgclass.github.io/prog1/spheres.json";
+  return connect(INPUT_SPHERES_URL);
 } // end get input spheres
 
-// Mathematics class for doing relavant work
-class Maths {
-  // Calc dot product of A&B == A.B
-  static dotproduct(x,y) {
-    return x[0]*y[0] + x[1]*y[1] + x[2]*y[2];
-  }
-  // Calc the magnitude of the vector
-  static magnitude(a) {
-    var mag = this.dotproduct(a,a);
-    if (mag < 0)
-      throw "magnitude can't be negative";
-    return Math.sqrt(mag);
-  }
-  // Calc the real roots for the equation ax2+bx+c=0
-  static realroots(a, b, c) {
-    return (b*b >= (4*a*c));
-  }
-  // Calc the least root of the equation ax2+bx+c=0
-  static leastroot(a,b,c) {
-    return (0 - (b + Math.sqrt((b*b - 4*a*c))))/(2*a);
-
-  }
-  // Calc A-B
-  static minus(a,b) {
-    return [a[0]-b[0], a[1]-b[1], a[2]-b[2]];
-  }
-  // Calc A+B
-  static add(a,b) {
-    return [a[0]+b[0], a[1]+b[1], a[2]+b[2]];
-  }
-  static mul(a,b) {
-    return [a*b[0], a*b[1], a*b[2]];
-  }
-
-  static unitvector(v1,v2) {
-    var ux = v2[0] - v1[0];
-    var uy = v2[1] - v1[1];
-    var uz = v2[2] - v1[2];
-    var mag = this.magnitude([ux, uy, uz]);
-    return [ux/mag, uy/mag, uz/mag];
-  }
-  // [A][B]
-  static mat_mul(A,B) {
-    console.log("mat mul");
-    console.log(A);
-    console.log(B);
-    var C = [];
-    for (var i=0; i<A.length; i++) {
-      C[i] = [];
-    }
-    for(var i = 0; i < A.length; i++) {
-      for(var j = 0; j < B.length; j++) {
-        var sum = 0;
-        for(var k = 0; k < A.length; k++) {
-          sum+=A[i][k]*B[j][k];
-        }
-        C[i][j] = sum;
-      }
-    }
-    return C;
-  }
-  // x & y are of form ax + by + c =0
-  static solvLinearEq(x,y) {
-    // [A][X] = [C]
-    var A = [[x[0],x[1]],[y[0],y[1]]];
-    var C = [[[-1*x[2]], [-1*y[2]]]];
-    var det_A = A[0][0]*A[1][1] - A[0][1]*A[1][0];
-    var A_inv = [[A[1][1],-1*A[0][1]],[-1*A[1][0],A[0][0]]];
-    var A_inv_math = math.inv();
-    var X = this.mul(det_A, this.mat_mul(A_inv, C));
-    return X;
-  }
-
-  static linepoint(p1,p2,t){
-    return (Maths.add(p1, Maths.mul(t, Maths.minus(p2,p1))));
-  }
+function getLights() {
+  const INPUT_LIGHTS_URL =
+        "https://ncsucgclass.github.io/prog1/lights.json";
+  // load the lights file
+  return connect(INPUT_LIGHTS_URL);
 }
-x
+
 function getInputTriangles() {
   const INPUT_TRIANGLES_URL =
         "https://raw.githubusercontent.com/riteshgajare/CSC561Projects/master/triangles.json";
-  // load the spheres file
-  var httpReq = new XMLHttpRequest(); // a new http request
-  httpReq.open("GET",INPUT_TRIANGLES_URL,false); // init the request
-  httpReq.send(null); // send the request
-  var startTime = Date.now();
-  while ((httpReq.status !== 200) && (httpReq.readyState !== XMLHttpRequest.DONE)) {
-    if ((Date.now()-startTime) > 3000)
-      break;
-  } // until its loaded or we time out after three seconds
-  if ((httpReq.status !== 200) || (httpReq.readyState !== XMLHttpRequest.DONE)) {
-    console.log*("Unable to open input triangle file!");
-    return String.null;
-  } else
-    return JSON.parse(httpReq.response);
+  // load the triangles file
+  return connect(INPUT_TRIANGLES_URL);
 }
 
 function raycasting(context) {
   var inputSpheres = getInputSpheres();
   var inputTriangles = getInputTriangles();
-  inputTriangles = String.null;
+  var lights = getLights();
   var black = new Color(0,0,0,255);
   var c = new Color(0,0,0,0); // the color at the pixel: black
   var w = context.canvas.width;
@@ -228,13 +157,15 @@ function raycasting(context) {
       var Ce = [inputSpheres[s].x, inputSpheres[s].y, inputSpheres[s].z];
       var E_C = Maths.minus(vp.eye, Ce);
       // For sphere : end
-      for (var p=0; p<numCanvasPixels; p++) {
+       for (var p=0; p<numCanvasPixels; p++) {
         if (s == 0) {
-          var px = Math.ceil(p / numCanvasHeightPixel);
+          var px = Math.floor(p / numCanvasHeightPixel);
           var py = p % numCanvasHeightPixel;
           var coord = [px, py];
           var real = vp.getRealCoord(coord);
-          pixels[p] = [real[0], real[1], real[2], px, py, 0];
+          // real[0] = vp.w/vp.h*real[0];
+          // real[1] = vp.h/vp.w*real[1];
+          pixels[p] = [real[0], real[1], real[2], w-px, py, 1233];
           //black background
           drawPixel(imagedata, Math.round(px), Math.round(py), black);
         }
@@ -244,36 +175,56 @@ function raycasting(context) {
         var B = 2* Maths.dotproduct(D, E_C);
         var C = Maths.dotproduct(E_C,E_C) - (inputSpheres[s].r*inputSpheres[s].r);
         if (Maths.realroots(A,B,C)) {
-          //drawPixel(imagedata, Math.round(pixels[p][3]), Math.round(pixels[p][4]), c);
+          if (!blinn_phong) {
+            c.change(inputSpheres[s].diffuse[0]*255, inputSpheres[s].diffuse[1]*255, inputSpheres[s].diffuse[2]*255, 255);
+            drawPixel(imagedata, Math.round(pixels[p][3]), Math.round(pixels[p][4]), c);
+            continue;
+          }
+          if (numCanvasPixels/2 == p)
+            break;
           var t = Maths.leastroot(A,B,C);
           //console.log(t);
-          if (t > pixels[p][5] && t > 1) {
+          if (t < pixels[p][5] && t > 1) {
             pixels[p][5] = t;
-            // Diffuse shading ==>
             var ps = Maths.add(vp.eye, Maths.mul(t,D));
-            var N = Maths.unitvector(Ce, ps);
-            var L = Maths.unitvector(ps, [2,4,-10.5]);
+            var N = Maths.unitvector(Ce,ps); //FIXME
+            var L = Maths.unitvector(ps,[2,4,-0.5]);
             var factor = Maths.dotproduct(N,L);
             if (factor > 1)
-              throw "factor > 1";
+              throw "factor > 1 not possible";
             var I = [0,0,0];
             I = Maths.add(I, inputSpheres[s].ambient);
             I = Maths.add(I, Maths.mul(factor, inputSpheres[s].diffuse));
             // <== End diffuse shading
             // Specular highlights ==>
             var R = Maths.minus(Maths.mul(2*factor,N), L);
-            var V = Maths.unitvector(ps, vp.eye);
-            factor = Math.pow(Maths.dotproduct(R,V), 5);
+            var V = Maths.unitvector(ps, vp.eye);//FIXME
+            //L = Maths.mul(-1,L);
+            var H = Maths.normalise(Maths.add(L,V));
+            factor = Math.pow(Maths.dotproduct(N,H), inputSpheres[s].n);
+            //factor = Math.pow(Maths.dotproduct(R,V), 5);
             if (factor > 1)
-              throw "factor > 1";
+              throw "factor > 1 not possible";
             I = Maths.add(I, Maths.mul(factor, inputSpheres[s].specular));
-            // Normalise or Max it out
+                     // External light sources
+            if (external_lighting) {
+              for(var ls = 0; ls < lights.length; ls++) {
+                L = Maths.unitvector(ps, [lights[ls].x, lights[ls].y, lights[ls].z]);
+                var factor = Maths.dotproduct(N,L);
+                I = Maths.add(I, math.dotMultiply(lights[ls].ambient, inputSpheres[s].ambient));
+                I = Maths.add(I, Maths.mul(factor, math.dotMultiply(lights[ls].diffuse, inputSpheres[s].diffuse)));
+                var H = Maths.normalise(Maths.add(L,V));
+                factor = Math.pow(Maths.dotproduct(N,H), inputSpheres[s].n);
+                I = Maths.add(I, Maths.mul(factor, math.dotMultiply(lights[ls].specular, inputSpheres[s].specular)));
+              } // Lights iterator
+            }
+            //Clamp intensity values
             for(var i = 0; i < 3; i++) {
               if (I[i] > 1)
-                I[i] = 0.9;
+                I[i] = 1;
               if (I[i] < 0)
-                I[i] = 0.034;
-            }
+                I[i] = 0.05;
+            } // Clamp intensity values
             I = Maths.mul(255, I);
             c.change(I[0], I[1], I[2], 255);
             // <== Specular highlights
@@ -285,59 +236,70 @@ function raycasting(context) {
   }
   // If triangle is found
   //  then:
-  if (inputTriangles != String.null) {
+  if (render_triangles && (inputTriangles != String.null)) {
     var n = inputTriangles.length;
-    console.log(inputTriangles);
     // Loop over the triangles, draw each in 2d
     // FIXME made s < 1 should be s<n
     for (var s=0; s<1; s++) {
       var vertices = inputTriangles[1].vertices;
       // For triangles
-      for (var p=0; p<numCanvasPixels; p++) {
+      pixel:for (var p=0; p<numCanvasPixels; p++) {
         var real_coord = [pixels[p][0], pixels[p][1], pixels[p][2]];
-        var dt = (vertices[0][2]-vp.eye[2])/(real_coord[2] - vp.eye[2]);
-        var tpts = Maths.linepoint(vp.eye, real_coord, dt);
+        // Ray from eye to pixel;
+        var v = Maths.minus(real_coord, vp.eye);
+        var intersect = Maths.line_plane_intersection(vertices[0],vertices[1],vertices[2],vp.eye,v);
+        // if (intersect == null)
+        //   console.log("Error");
         // using mathjs library for doing all the complex stuffs.
-        var Ax =  math.matrix([[(vertices[0][0] - vertices[2][0]),
-                                (vertices[1][0] - vertices[2][0])],
-                              [(vertices[0][1] - vertices[2][1]),
-                                (vertices[1][1] - vertices[2][1])]]);
-        var C = math.matrix([[vertices[2][0]], [vertices[2][1]]]);
-        var Ax_Inv = math.inv(Ax);
-        var sol = math.lusolve(Ax, C);
-        console.log(sol);
-        if (true) {
-          var t = Maths.leastroot(A,B,C);
-          //console.log(t);
-          if (t > pixels[p][5] && t > 1) {
-            pixels[p][5] = t;
-            // Diffuse shading ==>
-            // var ps = Maths.add(vp.eye, Maths.mul(t,D));
-            // var N = Maths.unitvector(Ce, ps);
-            // var L = Maths.unitvector(ps, [2,4,-0.5]);
-            // var factor = Maths.dotproduct(N,L);
-            var factor = 1;
-            var I = [0,0,0];
-            I = Maths.add(I, inputTriangles[s].material.ambient);
-            I = Maths.add(I, Maths.mul(factor, inputTriangles[s].material.diffuse));
-            // <== End diffuse shading
-            // Specular highlights ==>
-            // var R = Maths.minus(Maths.mul(2*factor,N), L);
-            // var V = Maths.unitvector(ps, vp.eye);
-            // factor = Math.pow(Maths.dotproduct(R,V), 5);
-            // I = Maths.add(I, Maths.mul(factor, inputTriangles[s].material.specular));
-            // // Normalise or Max it out
-            for(var i = 0; i < 3; i++) {
-              if (I[i] > 1)
-                I[i] = 0.9;
-              if (I[i] < 0)
-                I[i] = 0.034;
-            }
-            I = Maths.mul(255, I);
-            c.change(I[0], I[1], I[2], 255);
-            // <== Specular highlights
-            drawPixel(imagedata, Math.round(pixels[p][3]), Math.round(pixels[p][4]), c);
+        // var abc = Maths.barycentric_val(vertices[0],vertices[1],vertices[2],intersect);
+        // //console.log("a,b="+a+","+b);
+        if (intersect[2] != 0.25)
+          console.log("Error");
+        var abc = Maths.barycentric(vertices[0],vertices[1],vertices[2],intersect);
+        for(var i = 0; i < abc.length; i++) {
+          //console.log(bc);
+          if ((abc[i]<0) || (abc[i]>1))
+            continue pixel;
+        }
+        // var b = Maths.barycentric(vertices[1],vertices[2],vertices[0],intersect);
+        // var ab = Maths.barycentric(vertices[2],vertices[0],vertices[1],intersect);
+        // if (a>1 || a<0 )
+        //   continue pixel;
+        // if (b>1 || b<0 )
+        //   continue pixel;
+        // if (a+b+ab>1 || a+b+ab<0 )
+        //   continue pixel;
+        var t = Maths.magnitude(Maths.minus(intersect, vp.eye));
+        //console.log(t);
+        if (t <= pixels[p][5] && t > 0.5
+           ) {
+          pixels[p][5] = t;
+          // Diffuse shading ==>
+          var N = Maths.plane_normal(vertices[0],vertices[1],vertices[2]);
+          var L = Maths.unitvector(intersect, [2,4,-0.5]);
+          var factor = Maths.dotproduct(N,L);
+          var I = [0,0,0];
+          // if (intersect[0] < 0.5 && (real_coord[0] > 0.5))
+          //   continue pixel;
+          I = Maths.add(I, inputTriangles[s].material.ambient);
+          I = Maths.add(I, Maths.mul(factor, inputTriangles[s].material.diffuse));
+          // <== End diffuse shading
+          // Specular highlights ==>
+          var R = Maths.minus(Maths.mul(2*factor,N), L);
+          var V = Maths.unitvector(intersect, vp.eye);
+          factor = Math.pow(Maths.dotproduct(R,V), 5);
+          I = Maths.add(I, Maths.mul(factor, inputTriangles[s].material.specular));
+          // // Normalise or Max it out
+          for(var i = 0; i < 3; i++) {
+            if (I[i] > 1)
+              I[i] = 1;
+            if (I[i] < 0)
+              I[i] = 0;
           }
+          I = Maths.mul(255, I);
+          c.change(I[0], I[1], I[2], 255);
+          // <== Specular highlights
+          drawPixel(imagedata, Math.round(pixels[p][3]), Math.round(pixels[p][4]), c);
         }
       }
     }
@@ -346,19 +308,33 @@ function raycasting(context) {
 }
 /* main -- here is where execution begins after window load */
 
-function main() {
+function parse_val(id, def){
+  if (document.getElementById(id).value == "")
+    return def;
+  else
+    return parseFloat(document.getElementById(id).value);
+}
 
+function main(render) {
   // Get the canvas and context
   var canvas = document.getElementById("viewport");
   var context = canvas.getContext("2d");
-  var eye = [0.5, 0.5, -0.5];
-  var look_up = [0, 1, 0];
-  var look_at = [0, 0, 1];
+  var eye = [parse_val("x",0.5),parse_val("y", 0.5), parse_val("z",-0.5)];
+  console.log(eye);
+  canvas.width = parse_val("w", 512);
+  canvas.height = parse_val("h", 512);
+  if ((canvas.width==0) || canvas.height==0)
+    throw "canvas width/height cannot be 0"
+  external_lighting = (document.getElementById("e_lights").checked);
+  render_triangles = (document.getElementById("triangles").checked);
+  blinn_phong = (document.getElementById("blinn").checked);
+  var look_at = [parse_val("lx",0.5),parse_val("ly", 0.5), parse_val("lz",1)];
+  var look_up = [parse_val("vx",0),parse_val("vy", 1), parse_val("vz",0)];
   var d = 0.5;
-  var c = [0.5, 0.5, 0];
-  //Maths.solvLinearEq([5,3,1],[4,6,2]);
-  console.log(math.log(1000, 10));
-  vp = new ViewPane (canvas.width, canvas.height, eye, look_up, look_at, d, c);
+  var c = [0.5, 0.5, eye[2]+0.5];
+  vp = new ViewPane (canvas.width, canvas.height, eye, look_up, look_at, d, c, parse_val("asp_x", 1), parse_val("asp_y", 1));
   // Render image using ray casting techniques.
-  raycasting(context);
+  if (render == 1)
+    raycasting(context);
+  console.log("Rendering complete.");
 }
---
+++ maths.js
@@ -0,0 +1,104 @@
+// Mathematics class for doing relavant work
+class Maths {
+  // Calc dot product of A&B == A.B
+  static dotproduct(x,y) {
+    return x[0]*y[0] + x[1]*y[1] + x[2]*y[2];
+  }
+  // Calc the magnitude of the vector
+  static magnitude(a) {
+    var mag = this.dotproduct(a,a);
+    if (mag < 0)
+      throw "magnitude can't be negative";
+    return Math.sqrt(mag);
+  }
+  // Calc the real roots for the equation ax2+bx+c=0
+  static realroots(a, b, c) {
+    return (b*b >= (4*a*c));
+  }
+  // Calc the least root of the equation ax2+bx+c=0
+  static leastroot(a,b,c) {
+    var det = Math.sqrt(b*b - 4*a*c);
+    var t1 = (0 - (b + det))/(2*a);
+    var t2 = (0 - (b - det))/(2*a);
+    return ((t1<t2) ? t1 : t2);
+
+  }
+  // Calc A-B
+  static minus(a,b) {
+    return [a[0]-b[0], a[1]-b[1], a[2]-b[2]];
+  }
+
+  // Calc A+B
+  static add(a,b) {
+    return [a[0]+b[0], a[1]+b[1], a[2]+b[2]];
+  }
+  static mul(a,b) {
+    return [a*b[0], a*b[1], a*b[2]];
+  }
+
+  static normalise(v) {
+    var mag = this.magnitude(v);
+    return [v[0]/mag, v[1]/mag, v[2]/mag];
+  }
+
+  static unitvector(v1,v2) {
+    var u = this.minus(v2,v1);
+    return this.normalise(u)
+  }
+
+  static linepoint(p1,p2,t){
+    return (Maths.add(p1, Maths.mul(t, Maths.minus(p2,p1))));
+  }
+  //v1,v2,v3: vertices of triangle
+  static plane_normal(v1,v2,v3){
+    var v12 = this.unitvector(v1,v2);
+    var v23 = this.unitvector(v2,v3);
+    return this.normalise(math.cross(v12,v23));
+  }
+  //v1,v2,v3: vertices of triangle
+  //p: point from where ray origins
+  //v: direction vector of ray
+  static line_plane_intersection(v1,v2,v3,p,v){
+    var n = this.plane_normal(v1,v2,v3);
+    //v = this.normalise(v);
+    var w = this.minus(v1,p);
+    var k = this.dotproduct(w,n)/this.dotproduct(v,n);
+    if (k < 0)
+      throw "line plane wrong";
+    if (k>=1)
+      return (this.add(p,this.mul(k,v)));
+    else
+      console.log("Error");
+    return null;
+  }
+  // projection of a on b;
+  static projection(a,b){
+    return this.mul(this.dotproduct(a,b)/this.dotproduct(b,b), b);
+  }
+
+  static barycentric(a,b,c,p){
+    var ab = this.minus(a,b);
+    var cb = this.minus(c,b);
+    var ap = this.minus(a,p);
+    var cp = this.minus(c,p);
+    var bp = this.minus(b,p);
+    var v1 = this.minus(ab, this.projection(ab,cb));
+    var alpha = 1 - (this.dotproduct(v1,ap)/this.dotproduct(v1,ab));
+    var v2 = this.minus(cb, this.projection(cb,ab));
+    var beta = 1 - (this.dotproduct(v2,cp)/this.dotproduct(v2,cb));
+    var v3 = this.minus(cb, this.projection(cb,ab));
+    return [alpha, beta];
+  }
+
+  static barycentric_val(a,b,c,p){
+    var ab = this.minus(b,a);
+    var ac = this.minus(c,a);
+    var ap = this.minus(p,a);
+
+    return [this.projection(ap,ab), this.projection(ap,ac)];
+    //var cb = this.minus(c,b);
+    // var ap = this.minus(a,p);
+    // var v = this.minus(ab, this.projection(ab,cb));
+    // return (1 - (this.dotproduct(v,ap)/this.dotproduct(v,ab)));
+  }
+}
